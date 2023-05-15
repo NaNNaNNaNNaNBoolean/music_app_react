380408a32907f4a0ac925a1e3694ae02f35458ec
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-05-15 23:36:26 +0100

    So glad I had a quick look at it, knew I should of done a seperate function from the start, but I was too brain dead this afternoon to think

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import ReactDOM from 'react-dom/client'
 //import { useNavigate } from "react-router-dom";
 import SongList from './Songlist.jsx';
 import BandInfo from './Intro';
-import './App.css'
+import Form from './form.jsx';
+import './App.css';
+
 
 export default function App() {
   const [showing,setshow] = useState(false);
@@ -14,20 +16,18 @@ export default function App() {
   const showSongs = (e) => {
     e.preventDefault();
     setshow(!showing)
-    if (showing ){
+    if (showing){
+      //div.classList.toggle("visible");
       const songs = ReactDOM.createRoot(document.getElementById('songlist'));
       songs.render(<SongList/>);
-    }else{
-      //document.getElementById("songlist").remove()
     }
   }
   return (
     <>
       <BandInfo />
       <button onClick = {showSongs}>Show Songs</button>
-      <div id ="songlist">
-
-      </div>
+      <div id ="songlist"></div>
+      <Form />
     </>
   )
 }
--- src/Songlist.jsx
+++ src/Songlist.jsx
@@ -12,6 +12,19 @@ import  Bbp from './assets/images/boomboompow.jpg'
 import NotLiked from './assets/images/heart.png'
 import Liked from './assets/images/heartfull.png'
 
+function LikeState (){
+  const [isLiked,setIsLiked] = useState()
+ const likeMeHandler = () => {
+ setIsLiked(previsLiked => !previsLiked);
+ //e.target.
+ }
+ return(
+   <button  selected = {isLiked} onClick={likeMeHandler}>
+   {isLiked ? (<img id = "heart" src ={Liked}></img>) : (<img id="heart" src ={NotLiked}></img>)}
+   </button>
+ )
+}
+
 export default function SongList() {
   const songs = [
     { id: 1, img: Witl,name:"Where is the love?", relDate:2003, album: "Elephunk"},
@@ -21,20 +34,18 @@ export default function SongList() {
     {id: 5,img: Lgis,  name:"let's get it started", relDate:2004, album: "", liked: false },
     {id: 6,img: Bbp,  name:"Boon boom pow", relDate:2009, album: "The E.N.D", liked: false },
   ];
-  const [isLiked,setIsLiked] = useState(false)
-  
-  const likeMeHandler = () => {
-    setIsLiked(!isLiked);
-  }
  
+ 
+ 
+
   const songlist = songs.map(song =>
-    <div id ="list" key={song.id}>
-      <img src ={song.img}></img>
-      <dt>Song: {song.name}</dt> 
-        <dd >Release date:{song.relDate} Album:{song.album}</dd>
-      <button id ="likebtn" value = {isLiked} onClick={likeMeHandler}>
-      {isLiked ? (<img src ={Liked}></img>) : (<img  src ={NotLiked}></img>)}
-      </button>
+    <div id="list" key={song.id}>
+      <img id ="cover" src ={song.img}></img>
+      <div id ="songinfo">
+        <dt>Song: {song.name}</dt> 
+          <dd > Release date:{song.relDate} Album:{song.album}</dd>
+      </div>
+      <LikeState />
     </div>
   );
 
